c78dae38fb96c0d443f60d45c85abc815700b161
labhaoisemullins <77689895+labhaoisemullins@users.noreply.github.com>
2022-04-14 20:19:27 +0100

    first commit
    C++ Source File - command

---
+++ command.cpp
@@ -0,0 +1,44 @@
+/*
+ * CS4076 EDP Project
+ * Student ID: 20266928
+ * Filename: command.cpp
+*/
+
+#include "command.h" // include command header file
+
+/**
+ * Create a command object. First and second word must be supplied, but
+ * either one (or both) can be null. The command word should be null to
+ * indicate that this was a command that is not recognised by this game.
+ */
+/* initializer list */
+Command::Command(string firstWord, string secondWord) {
+    this->commandWord = firstWord;
+    this->secondWord = secondWord;
+}
+
+/**
+ * Return the command word (the first word) of this command. If the
+ * command was not understood, the result is null.
+ */
+string Command::getCommandWord() {
+    return this->commandWord;
+}
+
+/**
+ * Return the second word of this command.
+ * Returns null if there was no second word.
+ */
+string Command::getSecondWord() {
+    return this->secondWord;
+}
+
+/** Return true if this command was not understood. */
+bool Command::isUnknown() {
+    return (commandWord.empty());
+}
+
+/** Return true if the command has a second word. */
+bool Command::hasSecondWord() {
+    return (!secondWord.empty());
+}
