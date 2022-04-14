46855a4dbc893bccebe9e630cbf1231463aac042
labhaoisemullins <77689895+labhaoisemullins@users.noreply.github.com>
2022-04-14 20:44:36 +0100

    first commit

--- item.cpp
+++ item.cpp
@@ -1,47 +1,60 @@
-#include "item.h"
+/*
+ * CS4076 EDP Project
+ * Student ID: 20266928
+ * Filename: item.cpp
+*/
 
-Item::Item (string inDescription, int inWeightGrams, float inValue/**, int weaponCheck*/) {
-	description = inDescription;
-	setWeight(inWeightGrams);
-	value = inValue;
-	/**weaponCheck(isWeapon);*/
+#include "item.h"   // include item header file
+
+/* initializer list */
+Item::Item(string inDescription, int inWeightGrams, float inValue){
+    this->description = inDescription;
+    this->weightGrams = inWeightGrams;
+    this->value = inValue;
 }
 
-Item::Item(string inDescription) {
-	description = inDescription;
+/* initializer list ,  copy constructor */
+Item::Item(string inDescription){
+    this->description = inDescription;
+    this->weightGrams = 1;
+    this->value = 1;
 }
 
-void Item::setWeight(int inWeightGrams)
-{
+/*Operator Overloading */
+bool Item::operator==(Item &i){
+    int test = 0;
+
+    if(this->getShortDescription() == i.getShortDescription()){
+        test = 1;
+    }
+    return test;
+}
+
+/* set the weight */
+void Item::setWeight(int inWeightGrams){
     if (inWeightGrams > 9999 || inWeightGrams < 0)
-       cout << "weight invalid, must be 0<weight<9999" ;
-    else
-	   weightGrams = inWeightGrams;
+       cout << "weight invalid, must be 0 < weight < 9999." ;
+       else
+        weightGrams = inWeightGrams;
 }
 
-void Item::setValue(float inValue)
-{
+/* set the value */
+void Item::setValue(float inValue){
     if (inValue > 9999 || inValue < 0)
-       cout << "value invalid, must be 0<value<9999" ;
+       cout << "value invalid, must be 0 < value < 9999." ;
     else
-	   value = inValue;
+       value = inValue;
 }
 
-/**void Item::setWeaponCheck(int isWeapon)
-{
-    if(isWeapon > 0 || isWeapon < 0)
-        cout << "Item not a weapon" ;
-    else
-        cout << "Item is a weapon" ;
-}*/
-
-string Item::getShortDescription()
-{
-	return description;
+/* get weight function */
+int Item::getWeight(){
+    return weightGrams;
 }
 
-string Item::getLongDescription()
-{
-	return " item(s), " + description + ".\n";
+string Item::getShortDescription(){
+    return description;
 }
 
+string Item::getLongDescription(){
+    return description;
+}
