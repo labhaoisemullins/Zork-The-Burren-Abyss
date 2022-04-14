c02eb91c47161513d4e54e036d2852c04a84add8
labhaoisemullins <77689895+labhaoisemullins@users.noreply.github.com>
2022-04-14 20:48:58 +0100

    Update initialwindow.h

--- initialwindow.h
+++ initialwindow.h
@@ -1,38 +1,37 @@
+/* CS4076 EDP Project
+* Student ID: 20266928
+* Filename: initialwindow.h
+*/
 
-// * CS4076 EDP Project
-// * Student ID: 20266928
-// * Filename: initialwindow.h
-//*/
+/* Header guards to prevent header files being included numerous times */
+#ifndef INITIALWINDOW_H
+#define INITIALWINDOW_H
 
-///* Header guards to prevent header files being included numerous times */
-//#ifndef INITIALWINDOW_H
-//#define INITIALWINDOW_H
+#include "mainwindow.h"
 
-////#include "mainwindow.h"
+#include <QDialog>
 
-//#include <QDialog>
+namespace Ui {
+class initialwindow;
+}
 
-//namespace Ui {
-//class initialwindow;
-//}
+class initialwindow : public QDialog{
+   Q_OBJECT
 
-//class initialwindow : public QDialog{
-//    Q_OBJECT
+    friend class mainwindow;
 
-////    friend class mainwindow;
+public:
+   explicit initialwindow(QWidget *parent = nullptr);
+   ~initialwindow();   // Destructor
 
-//public:
-//    explicit initialwindow(QWidget *parent = nullptr);
-//    ~initialwindow();   // Destructor
+private slots:
+   void on_pushButton_clicked();   // start button
+   void on_pushButton_2_clicked(); // quit button
 
-////private slots:
-////    void on_pushButton_clicked();   // start button
-////    void on_pushButton_2_clicked(); // quit button
+private:
+   Ui::initialwindow *ui;
+   initialwindow *start;
+   initialwindow *quit;
+};
 
-//private:
-//    Ui::initialwindow *ui;
-// //   initialwindow *start;
-//   // initialwindow *quit;
-//};
-
-//#endif /* INITIALWINDOW_H
+#endif /* INITIALWINDOW_H
