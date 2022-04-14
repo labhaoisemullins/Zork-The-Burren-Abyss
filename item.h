caf1fd4ea3255d7cc1fbd15432f9c99d9e817ca7
labhaoisemullins <77689895+labhaoisemullins@users.noreply.github.com>
2022-04-14 20:41:06 +0100

    first commit

--- item.h
+++ item.h
@@ -1,30 +1,39 @@
+/*
+ * CS4076 EDP Project
+ * Student ID: 20266928
+ * Filename: item.h
+*/
+
+/* Header guards to prevent header files being included numerous times */
 #ifndef ITEM_H_
 #define ITEM_H_
 
+#include <iostream>
 #include <map>
 #include <string>
-#include <iostream>
-using namespace std;
 
-class Item {
+using namespace std;  // namespace
+
+class Item{
+/* access specifier: accessible only within the class */
 private:
-	string description;
-	string longDescription;
-	int weightGrams;
-	float value;
-	bool weaponCheck;
+    string description;
+    string longDescription;
+    int weightGrams;
+    float value;
 
+/* access specifier: accessible from outside the class */
 public:
-    Item (string description, int inWeight, float inValue);
-    Item (string description);
-	string getShortDescription();
+  //Item();
+    Item(string description, int weightGrams, float value);
+    Item(string description); // copy constructor
+    string getShortDescription();
     string getLongDescription();
-	int getWeight();
-	void setWeight(int weightGrams);
-	float getValue();
-	void setValue(float value);
-	int getWeaponCheck();
-	void setWeaponCheck(int weaponCheck);
+    bool operator==(Item &i);   // operator overloading
+    int getWeight();
+    void setWeight(int inWeightGrams);
+    float getValue();
+    void setValue(float inValue);
 };
 
-#endif /*ITEM_H_*/
+#endif /* ITEM_H_ */
