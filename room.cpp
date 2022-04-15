8bf50f33a157c87c896fe645bacde4e4c0f750a9
labhaoisemullins <77689895+labhaoisemullins@users.noreply.github.com>
2022-04-15 10:38:17 +0100

    final commit

--- room.cpp
+++ room.cpp
@@ -21,34 +21,35 @@ void Room::setExits(Room *up, Room *right, Room *down, Room *left) {    // point
     if(left != NULL)  exits["left"] = left;
 }
 
+/* return the short description */
 string Room::shortDescription(){
     return description;
 }
 
+/* return the long description */
 string Room::longDescription(){
     return "\ncurrent room: " + description + exitString();
 }
 
+/* displays the exits */
 string Room::exitString(){
     string returnString = "\nexits: ";
     for(map<string, Room*>::iterator i = exits.begin(); i != exits.end(); i++)
-    // Loop through map
+    // Loops through the map
         returnString += "  " + i->first;
     return returnString;
 }
 
+/* move to the next room if there is an exit */
 Room* Room::nextRoom(string direction) {
-    map<string, Room*>::iterator next = exits.find(direction); //returns an iterator for the "pair"
+    map<string, Room*>::iterator next = exits.find(direction);
     if(next == exits.end())
-        return nullptr; // if exits.end() was returned, there's no room in that direction.
-    return next->second; // If there is a room, remove the "second" (Room*)
-// part of the "pair" (<string, Room*>) and return it.
+        return nullptr;
+    return next->second;
 }
 
 /* add the item */
 void Room::addItem(Item *inItem) {
-    //cout << endl;
-    //cout << "Just added" + inItem->getLongDescription();
     itemsInRoom.push_back(*inItem);
 }
 
